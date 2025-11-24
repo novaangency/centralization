5aedbc68881bdc8ab3856c1c1c43a18611f4822f
3mg <140186933+sertr4linee@users.noreply.github.com>
2025-11-25 00:43:34 +0100

    refactor: simplify Home component by removing unnecessary useEffect and redirecting to login

--- www/app/page.tsx
+++ www/app/page.tsx
@@ -1,12 +1,6 @@
-"use client";
-
 import { redirect } from "next/navigation";
-import { useEffect } from "react";
 
 export default function Home() {
-  useEffect(() => {
-    redirect("/dashboard");
-  }, []);
-
-  return null;
+  // Middleware will handle the redirect
+  redirect("/auth/login");
 }
